49929e675288ee513f066a304b1d29901b1541ae
lrn80 <1349539656@qq.com>
2020-05-27 02:26:25 +0000

    feat: 用户退出登陆

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -12,6 +12,16 @@
                     </a>
 {{--                    <a class="nav-link" href="{{ route('users.show', Auth::user()) }}">个人中心</a>--}}
 {{--                    <a class="nav-link" href="#">编辑资料</a>--}}
+                    <a class="dropdown-item" href="{{ route('users.show', Auth::user()) }}">个人中心</a>
+                    <a class="dropdown-item" href="#">编辑资料</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" id="logout" href="#">
+                        <form action="{{ route('logout') }}" method="POST">
+                            {{ csrf_field() }}
+                            {{ method_field('DELETE') }}
+                            <button class="btn btn-block btn-danger" type="submit" name="button" >退出</button>
+                        </form>
+                    </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                         <a class="dropdown-item" href="{{ route('users.show', Auth::user()) }}">个人中心</a>
                         <a class="dropdown-item" href="#">编辑资料</a>
