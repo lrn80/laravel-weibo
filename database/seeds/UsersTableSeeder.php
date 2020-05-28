e46cd72d009f81d0e44d85ef89c7e8b2732565d8
lrn80 <1349539656@qq.com>
2020-05-28 01:54:52 +0000

    feat: 用户微博列表

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class UsersTableSeeder extends Seeder
 {
@@ -11,9 +12,8 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        $users = factory(User::class)->times(50)->make();
+        $users = factory(\App\Models\User::class)->times(50)->make();
         User::insert($users->makeVisible(['password', 'remember_token'])->toArray());
-
         $user = User::find(1);
         $user->name = 'Summer';
         $user->email = 'summer@example.com';
